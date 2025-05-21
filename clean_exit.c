0656f4c0d28cb0183da9361ca2a96c1c317de2a7
Tommaso Piana Agostinetti <topiana-@c2r1p7.42firenze.it>
2025-05-21 19:09:47 +0200

    clearing sprites

--- clean_exit.c
+++ clean_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   clean_exit.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: totommi <totommi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: topiana- <topiana-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 12:02:14 by topiana-          #+#    #+#             */
-/*   Updated: 2025/05/20 22:18:36 by totommi          ###   ########.fr       */
+/*   Updated: 2025/05/21 19:09:26 by topiana-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@ int clean_exit(t_mlx *mlx);
 int	clean_exit(t_mlx *mlx)
 {
 	//freeing mlx resources 
+	if (mlx->player.sprite != NULL)
+		mlx_destroy_image(mlx->mlx, mlx->player.sprite);	//for freeing sprites
 	mlx_destroy_window(mlx->mlx, mlx->win);
-	// mlx_destroy_display(mlx->mlx);	// macOS issues
+	mlx_destroy_display(mlx->mlx);	// macOS issues
 	free_mtx((void **)mlx->map.mtx);
 	free(mlx->mlx);
 	//finally exit
