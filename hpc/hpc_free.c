748abafe8c8f5b23a11c2e0813d2264a82a1a17a
Tommaso Piana Agostinetti <totommi@MACBOOKPRO-F0B2.station>
2025-06-11 02:25:53 +0200

    removed comment and useless variable

--- hpc/hpc_free.c
+++ hpc/hpc_free.c
@@ -6,7 +6,7 @@
 /*   By: totommi <totommi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 23:29:08 by totommi           #+#    #+#             */
-/*   Updated: 2025/06/11 02:21:10 by totommi          ###   ########.fr       */
+/*   Updated: 2025/06/11 02:25:39 by totommi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,11 +28,9 @@ all these wrapped in hpc and lbb mutex pls. */
 static void	shutdown_procedure(int *socket, int *index, t_player *lobby)
 {
 	char	buffer[MSG_LEN + 9];
-	int		my_socket;
 	int		my_index;
 
 	hpc_mutex(1);
-	my_socket = *socket;
 	my_index = *index;
 	*index = -1;
 	lbb_mutex(1);
@@ -40,7 +38,7 @@ static void	shutdown_procedure(int *socket, int *index, t_player *lobby)
 	{
 		ft_memset(buffer, 0, sizeof(buffer));
 		ft_strlcpy(buffer, "pls:just:die:fast:shutdown", sizeof(buffer));
-		if (sendto(my_socket, buffer, sizeof(buffer), 0,
+		if (sendto(*socket, buffer, sizeof(buffer), 0,
 			lobby[my_index].online, sizeof(struct sockaddr)) < 0)
 		{
 			ft_perror(ERROR"send failure"RESET);
@@ -67,14 +65,6 @@ int	hpc_free(int *socket, int *index, unsigned long thread)
 		shutdown_procedure(socket, index, lbb_get_ptr(NULL));
 	else if (index != NULL)
 		safewrite(index, -1);
-	// if (socket != NULL)
-	// {
-	// 	hpc_mutex(1);
-	// 	if (lbb_get_ptr(NULL)[*index])
-	// 	shutdown(*socket, SHUT_RDWR);
-	// 	close(*socket);
-	// 	hpc_mutex(2);
-	// }
 	if (tid && pthread_join(tid, NULL) != 0)
 		ft_printfd(2, "Error: join online thread\n");
 	server_sender(-1, NULL, NULL, 0);
