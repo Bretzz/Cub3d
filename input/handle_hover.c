63ef4fa5674c0c607716aa1b33acc68057a5b5e0
Tommaso Piana Agostinetti <topiana-@c2r4p8.42firenze.it>
2025-08-26 15:44:04 +0200

    video little fixes

--- input/handle_hover.c
+++ input/handle_hover.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   handle_hover.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: totommi <totommi@student.42.fr>            +#+  +:+       +#+        */
+/*   By: topiana- <topiana-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 12:16:31 by topiana-          #+#    #+#             */
-/*   Updated: 2025/06/10 12:56:15 by totommi          ###   ########.fr       */
+/*   Updated: 2025/08/26 15:42:52 by topiana-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,17 @@ int	leave_notify_handler(t_mlx *mlx);
 int	enter_notify_handler(t_mlx *mlx);
 
 // make the cursor visible // macOS issues
-// mlx_mouse_show(mlx->mlx, mlx->win);
 int	leave_notify_handler(t_mlx *mlx)
 {
+	mlx_mouse_show(mlx->mlx, mlx->win);
 	mlx->on_window = 0;
 	return (0);
 }
 
 // hide it again // macOS issues
-// mlx_mouse_hide(mlx->mlx, mlx->win);
 int	enter_notify_handler(t_mlx *mlx)
 {
+	mlx_mouse_hide(mlx->mlx, mlx->win);
 	mlx->on_window = 1;
 	return (0);
 }
--- input/update_frame.c
+++ input/update_frame.c
@@ -6,7 +6,7 @@
 /*   By: topiana- <topiana-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 21:53:26 by topiana-          #+#    #+#             */
-/*   Updated: 2025/06/18 17:37:40 by topiana-         ###   ########.fr       */
+/*   Updated: 2025/08/26 15:41:19 by topiana-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,17 @@ int	update_frame(void *arg)
 	t_mlx *const		mlx = (t_mlx *)arg;
 	static unsigned int	frame;
 
-	move_player(mlx);
-	move_mouse(mlx);
-	mlx->player.dir[0] = normalize_dir(mlx->player.dir[0]);
-	mlx->player.dir[1] = normalize_dir(mlx->player.dir[1]);
-	if (!put_board(mlx))
-		clean_exit(mlx, EXIT_FAILURE);
-	frame++;
+	if (frame++ % 11 == 0)
+	{
+		move_player(mlx);
+		move_mouse(mlx);
+		mlx->player.dir[0] = normalize_dir(mlx->player.dir[0]);
+		mlx->player.dir[1] = normalize_dir(mlx->player.dir[1]);
+		if (!put_board(mlx))
+			clean_exit(mlx, EXIT_FAILURE);
+	}
+	else
+		usleep(1000);
 	if (frame % 10 == 0)
 		mlx->fps = get_fps(frame);
 	return (0);
